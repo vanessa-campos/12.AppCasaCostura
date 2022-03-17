346f58770be058460b58b8a808b11f5f94b810a3
vanessa-campos <vanessa.c.oliveira93@gmail.com>
2022-03-17 18:26:36 -0300

    first commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject[] Panels;
+
+    public void ButtonMenu(){
+        
+    }
+    public void ButtonVender(){
+
+    }
+    public void ButtonCosturar(){
+        
+    }
+    public void ButtonProduto(){
+        
+    }
+    public void ButtonListaProdutos(){
+        
+    }
+    public void ButtonListaVendas(){
+        
+    }
+    public void ButtonListaCosturas(){
+        
+    }
+    public void ButtonListaResumo(){
+        
+    }
+
+
+
+}
